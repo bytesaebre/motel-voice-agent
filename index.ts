56757b60aa108b8e6348d9d8417afdf496c1493a
Rashid Taha Khan <rashid@MacBookPro.localdomain>
2026-05-18 20:04:49 -0400

    feat: add root health check endpoint to express server

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import WebhookRouter from "./src/router/webhooks";
 
 const app = express();
 
 app.use(express.json());
 app.use("/webhooks", WebhookRouter);
+
+app.get("/", (req: Request, res: Response) => {
+    console.log("Server is running");
+    res.send("Server is running");
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server started on port", process.env.PORT || 3000);
